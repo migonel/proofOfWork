f2c459519e118c5bc12841a861d90dcee28e2d76
Lucas <migonel55@gmail.com>
2023-02-20 01:27:10 -0300

    The Blockchain Address Update: - Updated generate private key and generate public key to use ECDSA algorithm - Created generate Blockchain Address function, it generates and addres using ECDSA from the previously generated keys.

--- wallet.js
+++ wallet.js
@@ -1,21 +1,35 @@
-// const crypto = require('crypto');
+const crypto = require('crypto');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-// function Wallet(blockchainAdress){
-//     this.funds = 0;
-//     this.blockchainAdress = blockchainAdress;
-//     this.privateKey = this.generatePrivateKey();
-//     this.publicKey = this.generatePublicKey(this.privateKey);
-// };
+function Wallet(blockchainAdress){
+    this.funds = 0;
+    this.privateKey = this.generatePrivateKey();
+    this.publicKey = this.generatePublicKey(this.privateKey);
+    this.blockchainAdress = this.generateBlockchainAdress(this.publicKey);
+};
 
-// Wallet.prototype.generatePrivateKey = function(){
-//     let privateKey = crypto.randomBytes(32).toString('hex');
-//     return privateKey;
-// };
+Wallet.prototype.generatePrivateKey = function() {
+  const keyPair = ec.genKeyPair();
+  const privateKey = keyPair.getPrivate('hex');
+  return privateKey;
+};
 
-// Wallet.prototype.generatePublicKey = function(privateKey) {
-//     return crypto.createHash('sha256').update(privateKey).digest('hex');
-// };
+Wallet.prototype.generatePublicKey = function(privateKey) {
+  const keyPair = ec.keyFromPrivate(privateKey);
+  const publicKey = keyPair.getPublic('hex');
+  return publicKey;
+};
 
-// let walletA = new Wallet();
-// console.log(walletA.privateKey);
-// console.log(walletA.publicKey);
+Wallet.prototype.generateBlockchainAdress = function(publicKey) {
+  const publicKeyBytes = Buffer.from(publicKey, 'hex');
+  const publicKeyHash = crypto.createHash('sha256').update(publicKeyBytes).digest();
+  const address = publicKeyHash.toString('hex');
+  return address;
+};
+
+let walletA = new Wallet("address");
+console.log(walletA.publicKey);
+console.log(walletA.privateKey);
+console.log(walletA.publicKey);
+console.log(walletA.blockchainAdress);
